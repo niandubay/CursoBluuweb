320b21ff61fd134ddb1230f6c949d009aad943ab
Niandubay <niandubay79@gmail.com>
2018-12-28 01:07:38 -0300

    Video 07
    Componentes

---
+++ componentes/saludos.js
@@ -0,0 +1,12 @@
+Vue.component('saludo', {
+    template: `
+    <div>
+    <h1>{{saludo}}</h1>
+    <h3>kljshlkdjhf</h3>
+    </div>`,
+    data(){
+        return {
+            saludo: 'Hola desde Vue'
+        }
+    }
+})
